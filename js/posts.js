8eef17d03c3ffe09d4f7b81e41afd4db0f93d4d8
TechDudie <73961295+TechDudie@users.noreply.github.com>
2022-01-11 13:43:34 -0600

    Update posts.js functions

--- js/posts.js
+++ js/posts.js
@@ -1,1 +1,21 @@
-
+function createXHR() {
+  try {
+    return new XMLHttpRequest();
+  } catch (e) {
+    try {
+      return new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (e) {
+      return new ActiveXObject("Msxml2.XMLHTTP");
+    }
+  }
+}
+function urlExists(testUrl) {
+  http = jQuery.ajax({
+    type: "HEAD",
+    url: testUrl,
+    async: false
+  })
+  status = http.status != 404;
+  delete http;
+  return status;
+}
