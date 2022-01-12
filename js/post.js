b1b6a7776d5bd167fccb73843202c828cdb51b45
TechDudie <73961295+TechDudie@users.noreply.github.com>
2022-01-12 09:15:25 -0600

    Update post.js

--- js/post.js
+++ js/post.js
@@ -22,23 +22,21 @@ function urlExists(testUrl) {
 i = 0
 while (true) {
   url = "https://raw.githubusercontent.com/TechDudie/soulthief/main/posts/post_" + i
-  if (urlExists(url)) {
-    newsfile = new createXHR();
-    newsfile.open('GET', url, false);
-    newsfile.send();
-    text = newsfile.responseText;
-    text = text.split("\n");
-    post = "<h3>" + text[0] + "</h3>";
-    text.shift();
-    text.forEach(function(item, index) {
-      post += "\n<p>" + item + "</p>";
-    });
-    document.getElementById("news").innerHTML += post;
-    delete newsfile;
-    delete text;
-    delete post;
-  } else {
+  newsfile = new createXHR();
+  newsfile.open('GET', url, false);
+  newsfile.send();
+  if (newsfile.status === 404) {
     break;
   }
+  text = newsfile.responseText;
+  text = text.split("\n");
+  post = "<h3>" + text[0] + "</h3>";
+  text.shift();
+  text.forEach(function(item, index) {
+    post += "\n<p>" + item + "</p>";
+  });
+  document.getElementById("news").innerHTML += post;
+  delete text;
+  delete post;
   i++;
 }
